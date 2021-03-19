f22f9f7941905a64ac88f313b34d7bc2b2cbe39d
Estelle <66007150+meijerestelle@users.noreply.github.com>
2021-03-19 15:37:20 +0100

    Initial Commit

---
+++ ThisMe/ThisMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThisMeApp.swift
+//  ThisMe
+//
+//  Created by Estèlle Meijer on 19/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ThisMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
